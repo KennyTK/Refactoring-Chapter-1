42b8bbe7a3c43900a40f6164904eaa67c64c11f9
Kenny <16214078+KennyTK@users.noreply.github.com>
2026-01-30 19:07:29 -0500

    Extract method getFrequentRenterPoints

--- RefactoringChapter1/src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ RefactoringChapter1/src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -29,11 +29,7 @@ public class Customer {
         
         for (Rental rental : rentals) 
         {
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += getFrequentRenterPoints(rental);
 
             // show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
@@ -45,5 +41,17 @@ public class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
         return result;
+    }
+    
+    private int getFrequentRenterPoints(Rental rental) 
+    {
+        int frequentRenterPoints = 0;
+        
+        frequentRenterPoints++;
+        
+        if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
+            frequentRenterPoints++;
+        
+        return frequentRenterPoints;
     }
 }
