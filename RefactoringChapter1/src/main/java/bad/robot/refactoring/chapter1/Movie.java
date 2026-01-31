3e9994bdf55326a90d1352422619cf37560581e5
Kenny <16214078+KennyTK@users.noreply.github.com>
2026-01-30 19:31:39 -0500

    Change priceCode to Price object

--- RefactoringChapter1/src/main/java/bad/robot/refactoring/chapter1/Movie.java
+++ RefactoringChapter1/src/main/java/bad/robot/refactoring/chapter1/Movie.java
@@ -7,11 +7,11 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String title;
-    private int priceCode;
+    private Price price;
 
     public Movie(String title, int priceCode) {
         this.title = title;
-        this.priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public String getTitle() {
@@ -19,12 +19,24 @@ public class Movie {
     }
     
     public void setPriceCode(int priceCode) {
-        this.priceCode = priceCode;
+        switch (priceCode) {
+        case CHILDREN:
+            price = new ChildrensPrice();
+            break;
+        case NEW_RELEASE:
+            price = new NewReleasePrice();
+            break;
+        case REGULAR:
+            price = new RegularPrice();
+            break;
+        default:
+            throw new IllegalArgumentException("invalid price code");
+    }
     }
     
     public int getFrequentRenterPoints(int daysRented) 
     {
-        if (priceCode == Movie.NEW_RELEASE && daysRented > 1)
+        if (price.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
             return 2;
         else
         	return 1;
@@ -33,7 +45,7 @@ public class Movie {
     public double getCharge(int daysRented) 
     {
         double amount = 0;
-        switch (priceCode) {
+        switch (price.getPriceCode()) {
             case REGULAR:
                 amount += 2;
                 if (daysRented > 2)
