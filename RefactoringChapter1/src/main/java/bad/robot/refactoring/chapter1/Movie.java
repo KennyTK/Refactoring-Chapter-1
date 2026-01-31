457a1c62607ab14602249cf6172291e329b49e1f
Kenny <16214078+KennyTK@users.noreply.github.com>
2026-01-30 19:46:02 -0500

    Move method getCharge

--- RefactoringChapter1/src/main/java/bad/robot/refactoring/chapter1/Movie.java
+++ RefactoringChapter1/src/main/java/bad/robot/refactoring/chapter1/Movie.java
@@ -44,23 +44,7 @@ public class Movie {
 
     public double getCharge(int daysRented) 
     {
-        double amount = 0;
-        switch (price.getPriceCode()) {
-            case REGULAR:
-                amount += 2;
-                if (daysRented > 2)
-                    amount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                amount += daysRented * 3;
-                break;
-            case CHILDREN:
-                amount += 1.5;
-                if (daysRented > 3)
-                    amount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return amount;
+    	return price.getCharge(daysRented);
     }
     
 }
